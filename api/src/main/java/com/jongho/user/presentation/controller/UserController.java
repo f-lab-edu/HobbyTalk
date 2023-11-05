f2045ecd734d2007f7bfa549e62e529f27039604
SeongJongHo <whdgh9595@gmail.com>
2023-11-05 22:34:53 +0900

    [MODIFY]git conflict 해결

--- api/src/main/java/com/jongho/user/presentation/controller/UserController.java
+++ api/src/main/java/com/jongho/user/presentation/controller/UserController.java
@@ -6,6 +6,7 @@ import com.jongho.user.application.dto.request.UserSignUpDto;
 import com.jongho.user.application.service.UserService;
 
 import lombok.RequiredArgsConstructor;
+<<<<<<< HEAD
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.http.ResponseEntity;
 
+=======
+import org.springframework.web.bind.annotation.*;
+
+>>>>>>> 6889d35 ([ADD]http request 파라미터 및 실행 메소드 로깅 advice & annotation 작성)
 @HttpRequestLogging
 @RestController
 @RequestMapping("/api/v1/users")
@@ -21,10 +26,16 @@ import org.springframework.http.ResponseEntity;
 public class UserController {
     private final UserService userService;
 
+<<<<<<< HEAD
     @PostMapping("/sign-up")
     public ResponseEntity<BaseResponseEntity<?>> signUp(@Validated @RequestBody UserSignUpDto userSignUpDto){
         userService.signUp(userSignUpDto);
 
         return BaseResponseEntity.ok("user create");
+=======
+    @GetMapping("/ping")
+    public BaseResponseEntity<?> ping(){
+        return BaseResponseEntity.ok(userService.ping());
+>>>>>>> 6889d35 ([ADD]http request 파라미터 및 실행 메소드 로깅 advice & annotation 작성)
     }
 }
--- common/src/main/java/com/jongho/advice/HttpRequestLoggingAdvice.java
+++ common/src/main/java/com/jongho/advice/HttpRequestLoggingAdvice.java
@@ -3,7 +3,6 @@ package com.jongho.advice;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,13 +12,14 @@ import java.util.Arrays;
 @Component
 @Aspect
 public class HttpRequestLoggingAdvice {
-    @Before("@within(com.jongho.annotaition.HttpRequestLogging)")
-    public void log(ProceedingJoinPoint joinPoint) throws Throwable {
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestLoggingAdvice.class);
+
+    @Around("@within(com.jongho.annotaition.HttpRequestLogging)")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
         String methodName = joinPoint.getSignature().toShortString();
         Object[] args = joinPoint.getArgs();
         logger.info("Method: " + methodName + ", parameters: " + Arrays.toString(args));
+
+        return joinPoint.proceed();
     }
-
-
-    private static final Logger logger = LoggerFactory.getLogger(HttpRequestLoggingAdvice.class);
 }
