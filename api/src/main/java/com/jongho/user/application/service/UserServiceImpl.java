7c6ab068d460895766648e6f70f49dbea2d20244
SeongJongHo <whdgh9595@gmail.com>
2023-11-07 20:42:13 +0900

    [MODIFY]UserService.signUp 예외처리 변경

--- api/src/main/java/com/jongho/user/application/service/UserServiceImpl.java
+++ api/src/main/java/com/jongho/user/application/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jongho.user.application.service;
 
+import com.jongho.common.exception.UserDuplicatedException;
 import com.jongho.user.application.dto.request.UserSignUpDto;
 import com.jongho.user.domain.model.User;
 import com.jongho.user.domain.repository.UserRepository;
@@ -15,11 +16,11 @@ public class UserServiceImpl implements UserService {
     public void signUp(UserSignUpDto userSignUpDto) {
         User user = userSignUpDto.toUser();
         if(userRepository.findOneByEmail(user.getEmail()).isPresent() ){
-            throw new RuntimeException("이미 존재하는 이메일입니다.");
+            throw new UserDuplicatedException("이미 존재하는 이메일입니다.");
         }
 
         if(userRepository.findOneByPhoneNumber(user.getPhoneNumber()).isPresent()){
-            throw new RuntimeException("이미 가입된 전화번호입니다.");
+            throw new UserDuplicatedException("이미 가입된 전화번호입니다.");
         }
 
         userRepository.create(user);
