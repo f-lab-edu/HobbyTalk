e8bf44fa0e09bd158d9fd22ef790f72548727c04
SeongJongHo <whdgh9595@gmail.com>
2023-11-07 20:37:36 +0900

    [ADD]전역 RestController 예외처리 적용 Advice 작성

---
+++ api/src/main/java/com/jongho/common/advice/ExceptionAdvice.java
@@ -0,0 +1,23 @@
+package com.jongho.common.advice;
+
+import com.jongho.common.exception.UserDuplicatedException;
+import com.jongho.response.BaseResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+    @ExceptionHandler(UserDuplicatedException.class)
+    public ResponseEntity<BaseResponseEntity<?>> handleUserDuplicatedException(UserDuplicatedException e) {
+        return getExceptionResponse(e.getMessage(), HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<BaseResponseEntity<?>> handleException(String message) {
+        return getExceptionResponse(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    private ResponseEntity<BaseResponseEntity<?>> getExceptionResponse(String message, HttpStatus status) {
+        return BaseResponseEntity.fail(status, message);
+    }
+}
--- api/src/main/java/com/jongho/user/presentation/controller/UserController.java
+++ api/src/main/java/com/jongho/user/presentation/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.jongho.user.presentation.controller;
 
 import com.jongho.annotaition.HttpRequestLogging;
+import com.jongho.common.exception.UserDuplicatedException;
 import com.jongho.response.BaseResponseEntity;
 import com.jongho.user.application.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @HttpRequestLogging
@@ -14,7 +16,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/ping")
-    public BaseResponseEntity<?> ping(){
-        return BaseResponseEntity.ok(userService.ping());
+    public ResponseEntity<?> ping(){
+        throw new UserDuplicatedException("이미 존재하는 유저입니다.");
     }
 }
