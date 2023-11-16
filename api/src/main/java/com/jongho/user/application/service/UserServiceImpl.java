c2108bc547450fe7f4ef0e6aca4aadd15ab158b3
SeongJongHo <whdgh9595@gmail.com>
2023-11-16 21:59:13 +0900

    [MODIFY]회원가입 로직 try catch문 추가

--- api/src/main/java/com/jongho/user/application/service/UserServiceImpl.java
+++ api/src/main/java/com/jongho/user/application/service/UserServiceImpl.java
@@ -2,9 +2,9 @@ package com.jongho.user.application.service;
 
 import com.jongho.common.exception.UserDuplicatedException;
 import com.jongho.user.application.dto.request.UserSignUpDto;
-import com.jongho.user.domain.model.User;
 import com.jongho.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -14,15 +14,19 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     @Override
     public int signUp(UserSignUpDto userSignUpDto) {
-        userRepository.findOneByUsername(userSignUpDto.getUsername())
-                .ifPresent((user)-> {
-                    throw new UserDuplicatedException("이미 존재하는 아이디입니다.");
-                });
+        try {
+            userRepository.findOneByUsername(userSignUpDto.getUsername())
+                    .ifPresent((user)-> {
+                        throw new UserDuplicatedException("이미 존재하는 아이디입니다.");
+                    });
 
-        userRepository.findOneByPhoneNumber(userSignUpDto.getPhoneNumber())
-                .ifPresent((user)-> {
-                    throw new UserDuplicatedException("이미 가입된 전화번호입니다.");
-                });
+            userRepository.findOneByPhoneNumber(userSignUpDto.getPhoneNumber())
+                    .ifPresent((user)-> {
+                        throw new UserDuplicatedException("이미 가입된 전화번호입니다.");
+                    });
+        } catch (DataIntegrityViolationException e) {
+            throw new UserDuplicatedException("이미 가입된 아이디거나 전화번호입니다.");
+        }
 
         return userRepository.createUser(userSignUpDto.toUser());
     }
