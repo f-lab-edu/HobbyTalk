a3eb12d4198003ab2f5faff5fc777fae9f2f54cf
SeongJongHo <whdgh9595@gmail.com>
2023-11-23 00:24:15 +0900

    [ADD] Category 관련 인티그레이션 테스트 및 유닛 테스트 작성

---
+++ api/src/test/java/com/jongho/user/application/service/CategoryServiceImplTest.java
@@ -0,0 +1,66 @@
+package com.jongho.user.application.service;
+
+import com.jongho.category.application.service.CategoryServiceImpl;
+import com.jongho.category.domain.model.Category;
+import com.jongho.category.domain.repository.CategoryRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("CategoryServiceImpl 클래스")
+public class CategoryServiceImplTest {
+    @Mock
+    private CategoryRepository categoryRepository;
+    @InjectMocks
+    private CategoryServiceImpl categoryServiceImpl;
+
+    @Nested
+    @DisplayName("getMainCategory 메소드는")
+    class Describe_getMainCategory {
+
+        @Test
+        @DisplayName("CategoryRepository.selectMainCategory()을 한번 호출하고 반환받은 값을 반환한다.")
+        void it_calls_CategoryRepository_selectMainCategory() {
+            // given
+            List<Category> expectCategories = List.of(new Category(1L, "main", 2L));
+            when(categoryRepository.selectMainCategory()).thenReturn(expectCategories);
+
+            // when
+            categoryServiceImpl.getMainCategory();
+
+            // then
+            verify(categoryRepository, times(1)).selectMainCategory();
+            assertEquals(expectCategories, categoryServiceImpl.getMainCategory());
+        }
+    }
+
+    @Nested
+    @DisplayName("getSubCategory 메소드는")
+    class Describe_getSubCategory {
+        @Test
+        @DisplayName("CategoryRepository.selectSubCategory()을 한번 호출하고 반환받은 값을 반환한다.")
+        void it_calls_CategoryRepository_selectSubCategory() {
+            // given
+            Long parentId = 1L;
+            List<Category> expectCategories = List.of(new Category(1L, "main", 2L));
+            when(categoryRepository.selectSubCategory(parentId)).thenReturn(expectCategories);
+
+            // when
+            categoryServiceImpl.getSubCategory(parentId);
+
+            // then
+            verify(categoryRepository, times(1)).selectSubCategory(parentId);
+            assertEquals(expectCategories, categoryServiceImpl.getSubCategory(parentId));
+        }
+    }
+}
---
+++ api/src/test/java/com/jongho/user/presentation/CategoryControllerTest.java
@@ -0,0 +1,84 @@
+package com.jongho.user.presentation;
+
+import com.google.gson.Gson;
+import com.jongho.category.application.service.CategoryService;
+import com.jongho.category.controller.CategoryController;
+import com.jongho.category.domain.model.Category;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CategoryController.class)
+@DisplayName("CategoryController 클래스")
+public class CategoryControllerTest {
+    @MockBean
+    private CategoryService categoryService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Nested
+    @DisplayName("getMainCategory 메소드는")
+    class Describe_getMainCategory {
+        @Test
+        @DisplayName("호출이 되면 status 200과 success라는 메세지를 반환한다.")
+        void it_returns_status_200_and_success_message() throws Exception{
+            // given
+            when(categoryService.getMainCategory()).thenReturn(List.of(new Category(1L, "main", 2L)));
+
+            // when
+            mockMvc.perform(get("/api/v1/categories/main")
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.message").value("success"))
+                    .andExpect(jsonPath("$.data").isArray())
+                    .andExpect(jsonPath("$.data[0].id").value(1))
+                    .andExpect(jsonPath("$.data[0].name").value("main"))
+                    .andExpect(jsonPath("$.data[0].parentId").value(2))
+                    .andDo(print());
+
+            // then
+            verify(categoryService, times(1)).getMainCategory();
+        }
+    }
+
+    @Nested
+    @DisplayName("getSubCategory 메소드는")
+    class Describe_getSubCategory {
+        @Test
+        @DisplayName("호출이 되면 status 200과 success라는 메세지를 반환한다.")
+        void it_returns_status_200_and_success_message() throws Exception{
+            // given
+            List<Category> expectedCategories = List.of(new Category(1L, "main", 2L));
+            when(categoryService.getSubCategory(2L)).thenReturn(expectedCategories);
+
+            // when
+            mockMvc.perform(get("/api/v1/categories/2/sub")
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.message").value("success"))
+                    .andExpect(jsonPath("$.data").isArray())
+                    .andExpect(jsonPath("$.data[0].id").value(1))
+                    .andExpect(jsonPath("$.data[0].name").value("main"))
+                    .andExpect(jsonPath("$.data[0].parentId").value(2))
+                    .andDo(print());
+
+            // then
+            verify(categoryService, times(1)).getSubCategory(2L);
+        }
+    }
+}
