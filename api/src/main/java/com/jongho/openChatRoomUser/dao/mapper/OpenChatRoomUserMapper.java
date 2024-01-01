0e81abb380ae3b8c63a887fbf64f50f07b679bfe
SeongJongHo <whdgh9595@gmail.com>
2024-01-01 22:03:07 +0900

    [ADD] 챗룸 유저 생성 관련 dao 로직 작성

---
+++ api/src/main/java/com/jongho/openChatRoomUser/dao/mapper/OpenChatRoomUserMapper.java
@@ -0,0 +1,9 @@
+package com.jongho.openChatRoomUser.dao.mapper;
+
+import com.jongho.openChatRoomUser.domain.model.OpenChatRoomUser;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OpenChatRoomUserMapper {
+    void createOpenChatRoomUser(OpenChatRoomUser openChatRoomUser);
+}
