7ea5f03c2273acc1a21716b230c2f3e20cdfcbf5
SeongJongHo <whdgh9595@gmail.com>
2024-01-01 23:36:48 +0900

    [MODIFY] 오픈챗룸 생성 facade에 category 조회 로직 추가

--- api/src/main/java/com/jongho/category/application/service/CategoryService.java
+++ api/src/main/java/com/jongho/category/application/service/CategoryService.java
@@ -3,8 +3,10 @@ package com.jongho.category.application.service;
 import com.jongho.category.domain.model.Category;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryService {
     public List<Category> getMainCategory();
     public List<Category> getSubCategory(Long parentId);
+    public Optional<Category> getOneCategoryById(Long categoryId);
 }
--- api/src/main/java/com/jongho/openChatRoom/application/facade/OpenChatRoomFacadeImpl.java
+++ api/src/main/java/com/jongho/openChatRoom/application/facade/OpenChatRoomFacadeImpl.java
@@ -1,5 +1,8 @@
 package com.jongho.openChatRoom.application.facade;
 
+import com.jongho.category.application.service.CategoryService;
+import com.jongho.common.exception.CategoryNotFoundException;
+import com.jongho.common.exception.MaxChatRoomsExceededException;
 import com.jongho.openChatRoom.application.dto.request.OpenChatRoomCreateDto;
 import com.jongho.openChatRoom.application.service.OpenChatRoomService;
 import com.jongho.openChatRoom.domain.model.OpenChatRoom;
@@ -14,12 +17,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class OpenChatRoomFacadeImpl implements OpenChatRoomFacade {
     private final OpenChatRoomService openChatRoomService;
     private final OpenChatRoomUserService openChatRoomUserService;
+    private final CategoryService categoryService;
     @Override
     @Transactional
     public void createOpenChatRoomAndOpenChatRoomUser(Long authUserId, OpenChatRoomCreateDto openChatRoomCreateDto) {
         if(openChatRoomService.getCountByManagerId(authUserId) >= 5){
-            throw new RuntimeException("최대 개설 가능한 채팅방 개수를 초과하였습니다.");
+            throw new MaxChatRoomsExceededException("최대 개설 가능한 채팅방 개수를 초과하였습니다.");
         }
+        categoryService.getOneCategoryById(openChatRoomCreateDto.getCategoryId()).orElseThrow(()->new CategoryNotFoundException("존재하지 않는 카테고리입니다."));
+
         OpenChatRoom openChatRoom = new OpenChatRoom(
                 openChatRoomCreateDto.getTitle(),
                 openChatRoomCreateDto.getNotice(),
