982b7ad4605c6a5b4cbb2940795c90865ea81988
SeongJongHo <whdgh9595@gmail.com>
2024-01-12 02:48:03 +0900

    [ADD] 오픈채팅 참여요청 관련 DAO로직 작성

---
+++ api/src/main/java/com/jongho/OpenChatRoomMembershipRequest/dao/mapper/OpenChatRoomMembershipRequestMapper.java
@@ -0,0 +1,12 @@
+package com.jongho.OpenChatRoomMembershipRequest.dao.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface OpenChatRoomMembershipRequestMapper {
+
+    public int countByRequesterIdAndStatus(@Param("requester_id") Long requesterId, @Param("status") int status);
+    public boolean existsByRequesterIdAndOpenChatRoomIdAndStatus(@Param("requester_id") Long requesterId, @Param("open_chat_room_id") Long openChatRoomId, @Param("status") int status);
+    public void createOpenChatRoomMembershipRequest(OpenChatRoomMembershipRequestMapper openChatRoomMembershipRequestMapper);
+}
---
+++ api/src/main/java/com/jongho/OpenChatRoomMembershipRequest/dao/repository/OpenChatRoomMembershipRequestMybatisRepositoryImpl.java
@@ -0,0 +1,28 @@
+package com.jongho.OpenChatRoomMembershipRequest.dao.repository;
+
+import com.jongho.OpenChatRoomMembershipRequest.dao.mapper.OpenChatRoomMembershipRequestMapper;
+import com.jongho.OpenChatRoomMembershipRequest.domain.repository.OpenChatRoomMembershipRequestRepository;
+import com.jongho.openChatRoom.domain.model.OpenChatRoom;
+import com.jongho.openChatRoom.domain.repository.OpenChatRoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class OpenChatRoomMembershipRequestMybatisRepositoryImpl implements OpenChatRoomMembershipRequestRepository {
+    private final OpenChatRoomMembershipRequestMapper openChatRoomMapper;
+    @Override
+    public int countByRequesterIdAndStatus(Long requesterId, int status) {
+        return openChatRoomMapper.countByRequesterIdAndStatus(requesterId, status);
+    }
+    @Override
+    public boolean existsByRequesterIdAndOpenChatRoomIdAndStatus(Long requesterId, Long openChatRoomId, int status) {
+        return openChatRoomMapper.existsByRequesterIdAndOpenChatRoomIdAndStatus(requesterId, openChatRoomId, status);
+    }
+    @Override
+    public void createOpenChatRoomMembershipRequest(OpenChatRoomMembershipRequestMapper openChatRoomMembershipRequestMapper) {
+        openChatRoomMapper.createOpenChatRoomMembershipRequest(openChatRoomMembershipRequestMapper);
+    }
+}
