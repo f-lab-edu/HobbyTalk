6b5d28acd63e4f6eed173148bc6c4f650f88eca7
SeongJongHo <whdgh9595@gmail.com>
2024-01-12 02:59:29 +0900

    [ADD] 오픈채팅 참여요청 관련 DAO로직 작성

--- api/src/main/java/com/jongho/OpenChatRoomMembershipRequest/application/service/OpenChatRoomMembershipRequestService.java
+++
@@ -1,9 +0,0 @@
-package com.jongho.OpenChatRoomMembershipRequest.application.service;
-
-import com.jongho.openChatRoom.domain.model.OpenChatRoom;
-
-import java.util.Optional;
-
-public interface OpenChatRoomMembershipRequestService {
-
-}
