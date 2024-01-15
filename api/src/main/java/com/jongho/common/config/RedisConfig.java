20ce4c1a226beaa5aa24f440672255810f64af79
SeongJongHo <whdgh9595@gmail.com>
2024-01-16 02:26:07 +0900

    [MODIFY] redis pub/sub용 컨테이너 추가

--- api/src/main/java/com/jongho/common/config/RedisConfig.java
+++ api/src/main/java/com/jongho/common/config/RedisConfig.java
@@ -4,9 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 @Configuration
 public class RedisConfig {
+    @Bean
+    public RedisMessageListenerContainer redisMessageListener(
+            RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+
+        return container;
+    }
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
 
--- api/src/main/java/com/jongho/common/util/redis/BaseRedisTemplate.java
+++ api/src/main/java/com/jongho/common/util/redis/BaseRedisTemplate.java
@@ -12,27 +12,28 @@ public class BaseRedisTemplate {
     private final StringRedisTemplate stringRedisTemplate;
     private static final ObjectMapper objectMapper = new ObjectMapper();
     public <T> void setData(String key, T value) {
-        String jsonValue;
-        try {
-            jsonValue = objectMapper.writeValueAsString(value);
-        }catch (Exception e) {
-            throw new MyJsonProcessingException(e.getMessage()!=null? e.getMessage():"json processing error");
-        }
-
-        stringRedisTemplate.opsForValue().set(key, jsonValue);
+        stringRedisTemplate.opsForValue().set(key, toJson(value));
     }
 
     public <T> T getData(String key, Class<T> valueType) {
         String jsonValue = stringRedisTemplate.opsForValue().get(key);
 
-        if (jsonValue != null) {
-            try {
-                return objectMapper.readValue(jsonValue, valueType);
-            }catch (Exception e) {
-                throw new MyJsonProcessingException(e.getMessage()!=null? e.getMessage():"json processing error");
-            }
-        } else {
-            return null;
+        return toObject(jsonValue, valueType);
+    }
+
+    public <T> T toObject(String json, Class<T> valueType) {
+        try {
+            return objectMapper.readValue(json, valueType);
+        }catch (Exception e) {
+            throw new MyJsonProcessingException(e.getMessage()!=null? e.getMessage():"json processing error");
+        }
+    }
+
+    public String toJson(Object value) {
+        try {
+            return objectMapper.writeValueAsString(value);
+        }catch (Exception e) {
+            throw new MyJsonProcessingException(e.getMessage()!=null? e.getMessage():"json processing error");
         }
     }
 }
